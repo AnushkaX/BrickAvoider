246e3e6c55603997665e483665e7fd522c4ab00b
AnushkaX <anushka.mutusinghe@gmail.com>
2021-06-09 23:30:36 +0530

    Initial Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sideWaysForce = 500f;
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sideWaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideWaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
