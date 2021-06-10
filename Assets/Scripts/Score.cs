b9fdfe969c90ff9753d22fa09b9eceec56dee1b5
AnushkaX <anushka.mutusinghe@gmail.com>
2021-06-10 18:53:44 +0530

    Fog and Score added

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
