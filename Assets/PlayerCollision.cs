cffba31673d54a252d9450bc116c750fb0a365a4
AnushkaX <anushka.mutusinghe@gmail.com>
2021-06-10 17:56:02 +0530

    obstacles added

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Obstacle")
+        {
+            Debug.Log("Hit the Obstacle");
+        }
+    }
+}
